2f449454603aaf4edc15291c386a055c63cf20bf
Prathama-1 <99348645+Prathama-1@users.noreply.github.com>
2025-05-02 21:49:23 +0530

    adding configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,19 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    // 'linebreak-style': 'off', // allow windows linebreaks
+    'object-curly-newline': 'off',
+    'quotes': 'off',
+    'no-undef': 'off',
+    'operator-linebreak': 'off',
+    'comma-dangle': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'spaced-comment': 'off',
+    'prefer-const': 'off',
+    'no-inner-declarations': 'off',
+    'prefer-template': 'off',
+    'no-dupe-keys': 'off',
+    'quote-props': 'off',
   },
 };
