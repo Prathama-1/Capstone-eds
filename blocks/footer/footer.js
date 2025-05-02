815dc49d2b4cb8da548fbdae9ad654e59e9b4ab3
Prathama-1 <99348645+Prathama-1@users.noreply.github.com>
2025-05-02 21:10:56 +0530

    removed eslint errors

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,5 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { getMetadata } from "../../scripts/aem.js";
+import { loadFragment } from "../fragment/fragment.js";
 
 /**
  * loads and decorates the footer
@@ -7,23 +7,25 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerMeta = getMetadata("footer");
+  const footerPath = footerMeta
+    ? new URL(footerMeta, window.location).pathname
+    : "/footer";
   const fragment = await loadFragment(footerPath);
 
-  block.textContent='';
-  const footerDivContainer=document.createElement('div');
-  const footerDivSpace=document.createElement('div');
-  footerDivSpace.classList.add('blank-space');
-  const footerClasses=['footer-nav','footer-text'];
-  let index=0;
-  while(fragment.firstElementChild){
+  block.textContent = "";
+  const footerDivContainer = document.createElement("div");
+  const footerDivSpace = document.createElement("div");
+  footerDivSpace.classList.add("blank-space");
+  const footerClasses = ["footer-nav", "footer-text"];
+  let index = 0;
+  while (fragment.firstElementChild) {
     fragment.firstElementChild.classList.add(footerClasses[index]);
-    footerDivContainer.append(fragment.firstElementChild)
-    if(index==0){
+    footerDivContainer.append(fragment.firstElementChild);
+    if (index === 0) {
       footerDivContainer.append(footerDivSpace);
     }
-    index+=1;
+    index += 1;
   }
   block.append(footerDivContainer);
 }
